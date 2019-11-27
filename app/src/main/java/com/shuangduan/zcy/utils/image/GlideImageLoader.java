b5a71f91c4e7ced030cc7141a90b34a041e66bdf
xuyu <coder_xyz@163.com>
2019-11-27 10:50:28 +0800

    1、修改首页Banner图片加载不出来的问题。

--- app/src/main/java/com/shuangduan/zcy/utils/image/GlideImageLoader.java
+++ app/src/main/java/com/shuangduan/zcy/utils/image/GlideImageLoader.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.blankj.utilcode.util.LogUtils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.shuangduan.zcy.R;
 import com.youth.banner.loader.ImageLoader;
 
@@ -44,11 +48,14 @@ public class GlideImageLoader extends ImageLoader {
                     .listener(new RequestListener<Drawable>() {
                         @Override
                         public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            LogUtils.e("加载失败");
                             return false;
                         }
 
                         @Override
                         public boolean onResourceReady(final Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            LogUtils.e("加载成功");
+                            imageView.setImageDrawable(resource);
                             return false;
                         }
                     })
