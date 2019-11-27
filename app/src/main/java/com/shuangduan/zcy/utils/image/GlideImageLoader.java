eaef2fca7289282ce993a71fcaab602e985284e4
xuyu <coder_xyz@163.com>
2019-11-27 09:40:55 +0800

    1、修改首页Banner 图片加载问题。

--- app/src/main/java/com/shuangduan/zcy/utils/image/GlideImageLoader.java
+++ app/src/main/java/com/shuangduan/zcy/utils/image/GlideImageLoader.java
@@ -1,8 +1,16 @@
 package com.shuangduan.zcy.utils.image;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.shuangduan.zcy.R;
 import com.youth.banner.loader.ImageLoader;
 
@@ -30,12 +38,23 @@ public class GlideImageLoader extends ImageLoader {
             imageView.setImageResource((Integer) path);
         }else if (path instanceof String){
             //Glide 加载图片简单用法
-            com.shuangduan.zcy.utils.image.ImageLoader.load(context, new ImageConfig.Builder()
-                    .url((String) path)
-                    .imageView(imageView)
-                    .errorPic(R.drawable.no_banner)
+
+            Glide.with(context)
+                    .load((String) path)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(final Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            return false;
+                        }
+                    })
                     .placeholder(R.drawable.no_banner)
-                    .build());
+                    .error(R.drawable.no_banner)
+                    .into(imageView);
         }
     }
 
